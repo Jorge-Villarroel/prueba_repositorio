065dd0c8197fa9b0f49f2b1815dca79a0254f72a
root <root@Jorge.localdomain>
2022-05-21 13:11:20 -0400

    agregando otro cambio

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/tv_shows', to: 'tv_shows#index'
+  get '/welcome', to: 'pages#welcome'
+  post '/tv_shows', to: 'tv_shows#create'
+ # root 'pages#welcome'
+end
